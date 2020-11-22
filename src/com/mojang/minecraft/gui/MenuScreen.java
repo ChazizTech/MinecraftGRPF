ce90611bc23fb81472c329de95198b18f6a7c15a
PF94 <45898787+flickPF94@users.noreply.github.com>
2020-11-22 01:32:51 -0500

    try and try and fail

--- src/com/mojang/minecraft/gui/MenuScreen.java
+++ src/com/mojang/minecraft/gui/MenuScreen.java
@@ -6,6 +6,10 @@ package com.mojang.minecraft.gui;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.lwjgl.opengl.GL11;
+
+import com.mojang.minecraft.renderer.Tesselator;
 import com.mojang.minecraft.renderer.Textures;
 import com.mojang.minecraft.gui.Screen;
 import com.mojang.minecraft.gui.Font;
@@ -61,6 +65,7 @@ public class MenuScreen extends Screen
     
     @Override
     public void render(final int xm, final int ym) {
+    	this.fill(0, 0, this.width, this.height, 537199872);
         for (int i = 0; i < this.buttons.size(); ++i) {
             final Button button = this.buttons.get(i);
             this.fill(button.x - 1, button.y - 1, button.x + button.w + 1, button.y + button.h + 1, -16777216);
