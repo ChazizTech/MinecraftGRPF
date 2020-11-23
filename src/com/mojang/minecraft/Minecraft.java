024d2b06d7121023c89fdc0a498b7fd402a8455d
PF94 <45898787+flickPF94@users.noreply.github.com>
2020-11-22 21:13:29 -0500

    Update Minecraft.java

--- src/com/mojang/minecraft/Minecraft.java
+++ src/com/mojang/minecraft/Minecraft.java
@@ -410,14 +410,14 @@ public class Minecraft implements Runnable, LevelLoaderListener
                     }
                     
                     if (Keyboard.getEventKey() == Keyboard.KEY_PERIOD) {
-                    	if (this.paintTexture != 12) {
+                    	if (this.paintTexture != 13) {
                             this.paintTexture += 1;
                         	}
                             	this.paintTexture = 1;
                         }
                     
                     if (Keyboard.getEventKey() == Keyboard.KEY_COMMA) {
-                    	if (this.paintTexture != 1) {
+                    	if (this.paintTexture != 0) {
                         this.paintTexture -= 1;
                     	}
                         	this.paintTexture = 12;
