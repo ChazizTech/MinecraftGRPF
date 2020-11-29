da24a929db0cd7ae206f21d2300337da3001a48a
PF94 <45898787+flickPF94@users.noreply.github.com>
2020-11-28 22:58:14 -0500

    Update Minecraft.java

--- src/com/mojang/minecraft/Minecraft.java
+++ src/com/mojang/minecraft/Minecraft.java
@@ -372,7 +372,6 @@ public class Minecraft implements Runnable, LevelLoaderListener {
 														// above the last
 														// available block.
 							this.paintTexture += 1;
-						this.font.drawShadow("MinecraftGRPF 0.1.1a", 7, 6, 16777215);
 						this.paintTextureString = ("Block Selected: " + this.paintTexture);
 					}
 
@@ -681,10 +680,6 @@ public class Minecraft implements Runnable, LevelLoaderListener {
 			this.screen.render(xMouse, yMouse);
 		}
 	}
-	
-    private void drawPauseText() {
-    	this.font.drawShadow("TESTY TEST, HELLO WORLD DUMBASS", 2, 22, 16777215);
-    }
 
 	private void setupFog(final int i) {
 		final Tile currentTile = Tile.tiles[Minecraft.level.getTile(
