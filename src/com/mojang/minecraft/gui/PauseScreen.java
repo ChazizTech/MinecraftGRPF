f50a54eecd68787c323b99f15afcbde6246a030a
PF94 <45898787+flickPF94@users.noreply.github.com>
2020-11-28 21:58:48 -0500

    a

--- src/com/mojang/minecraft/gui/PauseScreen.java
+++ src/com/mojang/minecraft/gui/PauseScreen.java
@@ -27,8 +27,8 @@ public class PauseScreen extends Screen
         this.buttons.add(new Button(0, this.width / 2 - 100, this.height / 4 + 0, 200, 20, "Generate new level"));
         //this.buttons.add(new Button(1, this.width / 2 - 100, this.height / 4 + 32, 200, 20, "Save level.."));
         //this.buttons.add(new Button(2, this.width / 2 - 100, this.height / 4 + 64, 200, 20, "Load level.."));
-        this.buttons.add(new Button(3, this.width / 2 - 100, this.height / 4 + 96, 100, 20, "Back to game"));
-        this.buttons.add(new Button(4, this.width / 2 - 25, this.height / 4 + 128, 100, 20, "Exit"));
+        this.buttons.add(new Button(3, this.width / 2 - 100, this.height / 4 + 96, 95, 20, "Back to game"));
+        this.buttons.add(new Button(4, this.width / 2 - 5, this.height / 4 + 96, 95, 20, "Exit"));
     }
     
     @Override
